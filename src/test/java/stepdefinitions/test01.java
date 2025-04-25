ff313c687c7f3ce94ab02bbcbec343a5f018defb
Burakuci <ucibrk@gmail.com>
2025-04-25 19:17:22 +0300

    Webhook test için test.txt eklendi

---
+++ src/test/java/stepdefinitions/test01.java
@@ -0,0 +1,21 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+
+public class test01 {
+
+    @Given("sistem hazır")
+    public void sistem_hazır() {
+        System.out.println("➡ Sistem hazır");
+    }
+
+    @When("kullanıcı test mesajını çalıştırır")
+    public void kullanıcı_test_mesajını_çalıştırır() {
+        System.out.println("➡ Test mesajı çalıştırılıyor...");
+    }
+
+    @Then("ekrana {string} yazısı yazdırılır")
+    public void ekrana_yazısı_yazdırılır(String mesaj) {
+        System.out.println("✅ " + mesaj);
+    }
+}
---
+++ src/test/resources/features/test.feature
@@ -0,0 +1,6 @@
+Feature: Basit Test
+
+  Scenario: Konsola yazı yazdırma testi
+    Given sistem hazır
+    When kullanıcı test mesajını çalıştırır
+    Then ekrana "Test başarıyla çalıştı" yazısı yazdırılır
